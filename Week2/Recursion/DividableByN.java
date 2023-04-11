ae5749370764f3ed93b8f8de240933f8ed40c80b
mohammedboreny <mohammed.boreny@gmail.com>
2023-04-11 03:22:30 +0300

    Added sumHolder to refresh sum value to 0 in every function call

--- Week2/Recursion/DividableByN.java
+++ Week2/Recursion/DividableByN.java
@@ -1,9 +1,12 @@
 
 public class DividableByN {
-    static int sum=0;
+     static int sum=0;
+     static int sumHolder=0;
     public static void main(String[] args) {
       int x=  sumOfDivideByN(100,7);
+        int y=  sumOfDivideByN(100,7);
         System.out.println("x = " + x);
+        System.out.println("y = " + y);
     }
 
     public static int sumOfDivideByN(int number,int divider) {
@@ -11,7 +14,9 @@ public class DividableByN {
         if (number > 0) {
           sum+=number%divider==0?number:0;
         }else {
-            return sum;
+            sumHolder=sum;
+            sum=0;
+            return sumHolder;
         }
         return sumOfDivideByN(--number, divider);
     }}
