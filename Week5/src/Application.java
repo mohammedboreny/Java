b84717e2b9f71c5139448d4fc6cd48f96b3c887c
mohammedboreny <mohammed.boreny@gmail.com>
2023-05-14 01:38:07 +0300

    I tried to flatten the array after mapping on it but i couldn't access the second diminsion to be able      *     to flatten mapTemp[i][j],

--- Week5/src/Application.java
+++ Week5/src/Application.java
@@ -1,13 +1,8 @@
 package Week5.src;
 
-import java.util.*;
-import java.util.concurrent.ExecutionException;
-import java.util.function.Function;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
+
+import java.util.Arrays;
 import java.util.stream.Stream;
-import static java.util.stream.Collectors.*;
-import static java.util.stream.Collectors.toList;
 
 public class Application {
 
@@ -21,73 +16,53 @@ public class Application {
                 Student.of("RUBA", 22, Gender.FEMALE, true, Course.MATH, Course.PHYSICS)
 
         };
-
-
-        System.out.println("studentsToList = " );
-        List<Student> studentsToList = toList(students);
-        System.out.println(studentsToList);
-        System.out.println("//Students -> List<Integer> using streams");
-        List<Student> studentsList = Stream.
-                of(students).collect(Collectors.toList());
-        studentsList.forEach(System.out::println);
-        System.out.println('\n');
-        System.out.println("// grouping student by age <20 using streams");
-        Map<Integer, List<Student>> studentsAboveAge_20_Grouping = Arrays.stream(students).filter(student -> student.getAge() < 20)
-                .collect(groupingBy(Student::getAge));
-        System.out.println("studentsAboveAge_20_Grouping = " + studentsAboveAge_20_Grouping+'\n');
-        System.out.println(" //grouping(keyExtractorFunction,valueExtractorFunction)->Map<Key,List<V>>");
-        Map<String, Integer> groupingByNameAndAge = Arrays.stream(students)
-                .collect(grouping(Student::getName, Student::getAge));
-        System.out.println("groupingByNameAndAge = " + groupingByNameAndAge);
+//         Stream.of(students).
+        ExpandedArray<Student> studentExpandedArray = ExpandedArray.of(students).flatMap(s -> Stream.of(s.getCourse()));
+        System.out.println(studentExpandedArray);
+//        List<Student> studentsToList = toList(students);
+//        System.out.println(studentsToList);
+//        System.out.println("//Students -> List<Integer> using streams");
+//        List<Student> studentsList = Stream.
+//                of(students).collect(Collectors.toList());
+//        studentsList.forEach(System.out::println);
+//        System.out.println('\n');
+//        System.out.println("// grouping student by age <20 using streams");
+//        Map<Integer, List<Student>> studentsAboveAge_20_Grouping = Arrays.stream(students).filter(student -> student.getAge() < 20)
+//                .collect(groupingBy(Student::getAge));
+//        System.out.println("studentsAboveAge_20_Grouping = " + studentsAboveAge_20_Grouping+'\n');
+//        System.out.println(" //grouping(keyExtractorFunction,valueExtractorFunction)->Map<Key,List<V>>");
+//        Map<String, Integer> groupingByNameAndAge = Arrays.stream(students)
+//                .collect(grouping(Student::getName, Student::getAge));
+//        System.out.println("groupingByNameAndAge = " + groupingByNameAndAge);
     }
 
     //toList->List implementation
-    public static <T> ArrayList<T> toList(T...data){
-
-        if (data==null ||data.length<1)
-            throw new IllegalArgumentException("Array is empty or length is < 1 ");
-        return reduceL(new ArrayList<T>(),
-                acc -> e -> {
-                    acc.add(e);
-                    return acc;
-                }, data);
-    }
+//    public static <T> ArrayList<T> toList(T...data){
+//
+//        if (data==null ||data.length<1)
+//            throw new IllegalArgumentException("Array is empty or length is < 1 ");
+//        return reduceL(new ArrayList<T>(),
+//                acc -> e -> {
+//                    acc.add(e);
+//                    return acc;
+//                }, data);
+//    }
 
 
     //grouping(keyExtractorFunction,valueExtractorFunction)->Map<Key,List<V>>
-    public static <T, K, U>
-    Collector<T, ?, Map<K,U>> grouping(Function<? super T, ? extends K> keyExtractorFunction,
-                                       Function<? super T, ? extends U> valueExtractorFunction){
-        return Collectors.toMap(keyExtractorFunction, valueExtractorFunction);
-    }
-
-
-
-    public static <U,T> U reduceR(U seed,Function<T,Function<U,U>> accFunction,T... data){
-        if(data==null || data.length==0){
-            return seed;
-        }
-        U result = seed;
-        for (int i = data.length - 1; i >= 0; i--) {
-            result=accFunction.apply(data[i]).apply(result);
-        }
-        return result;
-    }
-
-
-
-
-public static <U,T> U reduceL(U seed, Function<U,Function<T,U>> accFunction, T... data){
-
-        if(data == null || data.length==0){
-            return seed;
-        }
-        U accResult = seed;
-
-        for (int i = 0; i < data.length; i++) {
-            accResult=accFunction.apply(accResult).apply(data[i]);
-        }
-
-        return accResult;
-    }
+//    public <U,V> Map<U, ArrayDeque<V>> grouping(Function<T,U> keyExtractor,Function<T,V> valueExtractor){
+//        return reduceL(
+//                new HashMap<>(),
+//                acc->e->{
+//                    U key = keyExtractor.apply(e);
+//                    acc.compute(key, (k,l)->{
+//                        if(l == null){
+//                            l=new ArrayDeque<>();
+//                        }
+//                        l.addLast(valueExtractor.apply(e));
+//                        return l;
+//                    });
+//                    return acc;
+//                });
+//    }
 }
